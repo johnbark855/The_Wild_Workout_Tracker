31ac939dff653080277108d2b265ca5b48abdb00
john bark <johnbark855@gmail.com>
2021-03-23 19:01:26 -0400

    added and changed files

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default:() => new Date(),
+  },
+
+  exercises:
+   [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter the exercise type',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter the exercise name',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter the exercise duration in exact minutes',
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+module.exports = Workout;
